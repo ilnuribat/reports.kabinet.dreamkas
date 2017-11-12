fd1a716dfff0ac396b16200b0a4ab17ee067a8c0
ilnuribat <ilnuribat@gmail.com>
2017-11-12 03:25:26 +0300

    pretty print to chat, getting more info

--- src/webhooks/index.mjs
+++ src/webhooks/index.mjs
@@ -2,7 +2,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import validate from 'uuid-validate';
 import bodyparser from 'koa-bodyparser';
+import moment from 'moment';
 import bot from '../telegram';
+import * as kabinet from '../kabinetApi';
 
 const app = new Koa();
 const router = new Router();
@@ -18,9 +20,26 @@ router.post('/webhook/:token/:chatId', async (ctx) => {
 
     return;
   }
-
-  await telegram.sendMessage(chatId, `смена №${JSON.stringify(body)} закрыта`);
   ctx.status = 200;
+
+  if (body.action !== 'UPDATE' || body.type !== 'SHIFT') {
+    return;
+  }
+  const { data } = body;
+  const { deviceId, openedAt: from, closedAt: to } = data;
+  const deviceInfo = await kabinet.getDeviceInfo({ token, deviceId });
+  const { summary } = await kabinet.getReports({ token, device: deviceId, from, to });
+
+  await telegram.sendMessage(chatId, `Отчет от продажах за смену № ${data.shiftId}:
+    Кассир: ${data.cashier.name},
+    Время открытия: ${moment(data.openedAt).format('HH:mm')},
+    Время закрытия: ${moment(data.closedAt).format('HH:mm')},
+    Касса: ${deviceInfo.device},
+    Магазин: ${deviceInfo.shop},
+    Продажи:
+      Наличными: ${summary.cash.value},
+      Безнал: ${summary.cashless.value}
+  `);
 });
 
 app
