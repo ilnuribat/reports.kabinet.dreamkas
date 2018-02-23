4329f0aedb28490648c4666aa5120e10bcf36f8e
ilnuribat <ilnuribat@gmail.com>
2018-02-23 22:22:25 +0300

    by default 8080

--- src/index.mjs
+++ src/index.mjs
@@ -2,5 +2,5 @@ import bot from './telegram';
 import app from './webhooks';
 
 bot.startPolling();
-app.listen(process.env.HTTP_PORT);
+app.listen(process.env.HTTP_PORT || 8080);
 console.log('bot started');
